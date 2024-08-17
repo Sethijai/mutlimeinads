6d0af02af89932ef87138b88abe4b4e2c7e5b9d6
Sethijai <148748128+Sethijai@users.noreply.github.com>
2024-08-17 09:19:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", " ")
+START_MSG = os.environ.get("START_MESSAGE", "")
 try:
     ADMINS=[6376328008]
     for x in (os.environ.get("ADMINS", "5487643307").split()):
@@ -50,7 +50,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", " ")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "")
