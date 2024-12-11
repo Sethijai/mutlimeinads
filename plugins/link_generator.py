f34f38f2b164f82c3a25c13890d5d09a81e6b256
Sethijai <148748128+Sethijai@users.noreply.github.com>
2024-12-11 18:42:40 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -35,7 +35,10 @@ async def batch(client: Client, message: Message):
 
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
+    if WEBSITE_URL_MODE == True:
+        link = f"{WEBSITE_URL}?HACKHEIST={base64_string}"
+    else:
+        link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await second_message.reply_text(f"<b>Here is your link</b>\n\n<code>{link}</code>", quote=True, reply_markup=reply_markup)
 
@@ -55,6 +58,9 @@ async def link_generator(client: Client, message: Message):
             continue
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
-    link = f"https://t.me/{client.username}?start={base64_string}"
+    if WEBSITE_URL_MODE == True:
+        link = f"{WEBSITE_URL}?HACKHEIST={base64_string}"
+    else:
+        link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await channel_message.reply_text(f"<b>Here is your link</b>\n\n<code>{link}</code>", quote=True, reply_markup=reply_markup)
