bfef7503128e308c044d135bc5143b6fca1faf6c
Sethijai <148748128+Sethijai@users.noreply.github.com>
2025-01-31 14:11:12 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -4,7 +4,7 @@ from bot import Bot
 from config import ADMINS
 from helper_func import encode, get_message_id
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('bulk'))
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def bulk(client: Client, message: Message):
     try:
         bulk_input = await client.ask(
