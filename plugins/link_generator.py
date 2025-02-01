5bb38e75ab54e80a74b0298399061b4a463deedc
Sethijai <148748128+Sethijai@users.noreply.github.com>
2025-02-01 19:23:21 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -6,10 +6,6 @@ from helper_func import encode
 
 import re
 
-$ = {
-₹ = '
-! = }
-
 def extract_message_id(url):
     match = re.search(r'/(\d+)$', url)
     return int(match.group(1)) if match else None
@@ -58,7 +54,7 @@ async def bulk(client: Client, message: Message):
         base64_string = await encode(string)
         batch_link = f"https://t.me/{client.username}?start={base64_string}"
 
-        response_text += f"$ text: ₹{subject}₹, url: ₹{batch_link}₹ !,\n"
+        response_text += f"{ text: '{subject}', url: '{batch_link}' },\n"
 
     # Send response
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={batch_link}')]])
