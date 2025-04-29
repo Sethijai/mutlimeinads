e43024e7abc62073b86dc667c1cf7f33c7963dd1
Sethijai <148748128+Sethijai@users.noreply.github.com>
2025-04-29 10:02:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001644866777"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "18,000")) # auto delete in seconds
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "18000")) # auto delete in seconds
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "")
