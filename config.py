e7db7f9a9188728e60bf4b6f4586f73528f4b067
Sethijai <148748128+Sethijai@users.noreply.github.com>
2025-04-29 12:54:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{previous_caption}</b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{previouscaption}</b>")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
