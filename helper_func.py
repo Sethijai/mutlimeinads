b8bd64c61bc1650f7998a142d79548c1ad15914d
Sethijai <148748128+Sethijai@users.noreply.github.com>
2025-05-19 16:57:54 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -3,11 +3,13 @@
 import base64
 import re
 import asyncio
+from typing import Tuple
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
 from config import FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
+
 
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL:
