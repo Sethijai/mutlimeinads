a3b3528801a08fdf389a11617e4504cbcba35f44
Sethijai <148748128+Sethijai@users.noreply.github.com>
2025-09-23 19:38:36 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,13 +1,13 @@
+# database.py
 import pymongo, os
 from config import DB_URI, DB_NAME
 
 dbclient = pymongo.MongoClient(DB_URI)
 database = dbclient[DB_NAME]
 user_data = database['users']
+random_messages = database['random_messages']  # Collection for random message IDs
 
-
-
-async def present_user(user_id : int):
+async def present_user(user_id: int):
     found = user_data.find_one({'_id': user_id})
     return bool(found)
 
@@ -20,9 +20,20 @@ async def full_userbase():
     user_ids = []
     for doc in user_docs:
         user_ids.append(doc['_id'])
-        
     return user_ids
 
 async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
     return
+
+async def add_random_message(channel_id: str, msg_id: int):
+    random_messages.insert_one({'channel_id': channel_id, 'msg_id': msg_id})
+    return
+
+async def remove_random_message(channel_id: str, msg_id: int):
+    random_messages.delete_one({'channel_id': channel_id, 'msg_id': msg_id})
+    return
+
+async def get_all_random_messages():
+    messages = random_messages.find()
+    return [(doc['channel_id'], doc['msg_id']) for doc in messages]
