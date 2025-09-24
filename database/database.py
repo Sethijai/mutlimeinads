98720c72fa5ab32cd1a337d1b608b2b3318f82a1
Sethijai <148748128+Sethijai@users.noreply.github.com>
2025-09-24 06:38:32 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,11 +1,10 @@
-# database.py
 import pymongo, os
 from config import DB_URI, DB_NAME
 
 dbclient = pymongo.MongoClient(DB_URI)
 database = dbclient[DB_NAME]
 user_data = database['users']
-random_messages = database['random_messages']  # Collection for random message IDs
+special_messages = database['special_messages']
 
 async def present_user(user_id: int):
     found = user_data.find_one({'_id': user_id})
@@ -26,14 +25,30 @@ async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
     return
 
-async def add_random_message(channel_id: str, msg_id: int):
-    random_messages.insert_one({'channel_id': channel_id, 'msg_id': msg_id})
+async def add_special_message(msg_id: int):
+    """
+    Add a message ID to the special messages collection.
+    """
+    special_messages.update_one(
+        {'_id': 'special_msg_ids'},
+        {'$addToSet': {'msg_ids': msg_id}},
+        upsert=True
+    )
     return
 
-async def remove_random_message(channel_id: str, msg_id: int):
-    random_messages.delete_one({'channel_id': channel_id, 'msg_id': msg_id})
+async def remove_special_message(msg_id: int):
+    """
+    Remove a message ID from the special messages collection.
+    """
+    special_messages.update_one(
+        {'_id': 'special_msg_ids'},
+        {'$pull': {'msg_ids': msg_id}}
+    )
     return
 
-async def get_all_random_messages():
-    messages = random_messages.find()
-    return [(doc['channel_id'], doc['msg_id']) for doc in messages]
+async def get_special_messages():
+    """
+    Retrieve all special message IDs.
+    """
+    doc = special_messages.find_one({'_id': 'special_msg_ids'})
+    return doc['msg_ids'] if doc and 'msg_ids' in doc else []
